c5161effd017a2866375e079a9640bdfcaabd73d
21120572 <117740701+NguDuyTinh@users.noreply.github.com>
2024-12-19 17:49:09 +0700

    Update and rename sample-react-app.js to sample-react-app

--- DevOps/sample-react-app.js
+++
@@ -1,9 +0,0 @@
-function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Welcome to My Sample React App!</h1>
-        </header>
-      </div>
-    );
-  }
