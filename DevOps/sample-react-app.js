2e71582958b252c7e7b2296b4afa95de5bffd010
21120572 <117740701+NguDuyTinh@users.noreply.github.com>
2024-12-19 17:29:27 +0700

    Add files via upload

---
+++ DevOps/sample-react-app.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to My Sample React App!</h1>
+        </header>
+      </div>
+    );
+  }
